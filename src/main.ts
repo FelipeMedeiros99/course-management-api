e5d58d6f08229b8751ee4e185739435f2a6a0768
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2024-09-14 02:31:44 -0300

    bug de porta resolvido

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import dotenv from "dotenv"
+import * as dotenv from "dotenv"
 
+dotenv.config()
+const {PORT} = process.env
+
+console.log(PORT)
 async function bootstrap() {
-  dotenv.config()
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
@@ -12,6 +15,6 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
   }));
   app.enableCors();
-  await app.listen(process.env.PORT||3000);
+  await app.listen(PORT||3000, ()=>{console.log("servidor funcionando na porta ", process.env.PORT)});
 }
 bootstrap();
