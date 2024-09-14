33661331ab6d45087acd9801b64250a8e7852b36
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2024-09-14 02:21:58 -0300

    definindo porta

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import dotenv from "dotenv"
 
 async function bootstrap() {
-  dotenv.config
+  dotenv.config()
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
