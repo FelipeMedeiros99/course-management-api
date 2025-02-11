ed5556706a5d462a3f11bee151ae610283ab95bd
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-02-11 01:01:36 -0300

    refactor: reestruturing userModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
-import { DatabaseModule } from './database/database.module';
-import { CursoController } from './curso/curso.controller';
-import { CursoService } from './curso/curso.service';
-import { CarrinhoController } from './carrinho/carrinho.controller';
-import { CarrinhoService } from './carrinho/carrinho.service';
 import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { PrismaService } from './config/prisma.service';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-    imports: [ConfigModule.forRoot({
-    envFilePath: ['.env'],
-    isGlobal: true
-  })],
-  controllers: [AppController],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true
+    })],
+    
+  controllers: [AppController, PrismaService],
   providers: [AppService],
-  // imports: [DatabaseModule, ConfigModule.forRoot({
-  //   envFilePath: ['.env'],
-  //   isGlobal: true
-  // })],
-  // controllers: [AppController, UserController, CursoController, CarrinhoController],
-  // providers: [AppService, UserService, CursoService, CarrinhoService],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import * as dotenv from "dotenv"
-
-dotenv.config()
-
-const {PORT} = process.env
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(new ValidationPipe({
-  //   transform: true,
-  //   whitelist: true,
-  //   forbidNonWhitelisted: true,
-  // }));
+ 
   app.enableCors();
-  await app.listen(PORT||5000, ()=>{console.log("server running")});
+ 
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
+ 
+  await app.listen(process.env.PORT, ()=>{console.log("server running")});
 }
 bootstrap();
