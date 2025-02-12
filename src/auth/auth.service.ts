91cf82ac181932e139d7dc73bbc46f3fd87ee111
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-02-12 20:43:42 -0300

    feat: logger added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client';
 import * as bcrypt from "bcrypt";
@@ -9,6 +9,7 @@ import { SignInUserDto, SignUpUserDto } from 'src/dto/auth.dto';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name)
   constructor(
     private prisma: PrismaService,
     private jwtService: JwtService
@@ -31,48 +32,63 @@ export class AuthService {
   }
 
   async encryptPassword(password: string) {
-    const SALTS = Number(process.env.SALTS_ROUNDS)||10;
+    const SALTS = Number(process.env.SALTS_ROUNDS) || 10;
     const encryptedPassword = await bcrypt.hash(password, SALTS);
     return encryptedPassword;
   }
 
-  async validEncryptedPassword(password: string, encryptedPassword: string){
+  async validEncryptedPassword(password: string, encryptedPassword: string) {
     const isCorrectPassword = await bcrypt.compare(password, encryptedPassword);
     return isCorrectPassword;
   }
 
-  async generateToken(userData: Omit <User, "password">){
+  async generateToken(userData: Omit<User, "password">) {
     const token = this.jwtService.signAsync(userData)
     return token
   }
 
   async signUpUser(userData: SignUpUserDto) {
-    const { email, name, password, confirmPassword } = userData;
-    const userAlredyExist = await this.findUser(email);
+    try {
+      const { email, name, password, confirmPassword } = userData;
+      const userAlredyExist = await this.findUser(email);
 
-    if (userAlredyExist) throw new HttpException("Email already existe", 409)
-    if (password !== confirmPassword) throw new HttpException("Passwords don't match", 400)
-    
-    const encryptedPassword = await this.encryptPassword(password)
+      if (userAlredyExist) throw new HttpException("Email already existe", 409);
+      if (password !== confirmPassword) throw new HttpException("Passwords don't match", 400);
 
-    await this.saveUser({
-      email: email.toLocaleLowerCase(),
-      name: name.toLocaleUpperCase(),
-      password: encryptedPassword
-    })
+      const encryptedPassword = await this.encryptPassword(password);
+
+      await this.saveUser({
+        email: email.toLocaleLowerCase(),
+        name: name.toLocaleUpperCase(),
+        password: encryptedPassword
+      });
+    } catch (e) {
+      if (e instanceof HttpException) throw e;
+
+      this.logger.error(e)
+      throw new HttpException("error when registering", 500)
+    }
   }
 
   async signinUser(userData: SignInUserDto) {
-    const {email, password} = userData
-    
-    const userDatabase = await this.findUser(userData.email)
-    if (!userDatabase) throw new HttpException("Email not registered", 401)
+    try {
+      const { email, password } = userData;
 
-    const isCorrectPassword = await this.validEncryptedPassword(password, userDatabase.password)
-    if(!isCorrectPassword) throw new HttpException("Incorrect password", 401)
-    
-    const token = await this.generateToken({id: userDatabase.id, email, name: userDatabase.email})
-    return token
+      const userDatabase = await this.findUser(userData.email);
+      if (!userDatabase) throw new HttpException("Email not registered", 401);
+
+      const isCorrectPassword = await this.validEncryptedPassword(password, userDatabase.password);
+      if (!isCorrectPassword) throw new HttpException("Incorrect password", 401);
+
+      const token = await this.generateToken({ id: userDatabase.id, email, name: userDatabase.email });
+      return token;
+
+    } catch (e) {
+      if (e instanceof HttpException) throw e;
+      this.logger.error(e);
+      throw new HttpException("error logging in", 500);
+
+    }
   }
 
 }
--- src/courses/course.controller.ts
+++ src/courses/course.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, Post, Body, Put, UseGuards } from "@nestjs/common";
+import { Controller, Get, HttpCode, Post, Body, Put, UseGuards, HttpException, Logger } from "@nestjs/common";
 import { CourseService } from "./course.service";
 import { CourseDataDto, EditCourseDto } from "src/dto/course.dto";
 import { AuthGuard } from "src/auth/auth.guard";
@@ -6,24 +6,40 @@ import { AuthGuard } from "src/auth/auth.guard";
 @UseGuards(AuthGuard)
 @Controller("courses")
 export class CourseController{
-    constructor(private readonly courseService: CourseService){ }
+    private readonly logger = new Logger(CourseController.name);
+    constructor(private readonly courseService: CourseService){ };
 
     @Get()
     async getCourse(){
-      return this.courseService.getCourse()
+      try{
+        return await this.courseService.getCourse()
+      }catch(e){
+        this.logger.error(e)
+        throw new HttpException("Error searching for courses", 500)
+      }
     }
 
     @HttpCode(201)
     @Post()
     async addCourse(@Body() courseData: CourseDataDto){
-      await this.courseService.addCourse(courseData)
-      return "Created"
+      try{
+        await this.courseService.addCourse(courseData)
+        return "Created"
+      }catch(e){
+        this.logger.error(e)
+        throw new HttpException("Error trying to create course", 500)
+      }
     }
 
     @HttpCode(202)
     @Put()
     async editCourse(@Body() courseData: EditCourseDto){
-      await this.courseService.editCourse(courseData)
-      return "Altered"
+      try{
+        await this.courseService.editCourse(courseData)
+        return "Altered"
+      }catch(e){
+        this.logger.error(e)
+        throw new HttpException("error editing course", 500)
+      }
     }
 }
