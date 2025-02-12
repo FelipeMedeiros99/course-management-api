43b5021a7319144ec11a243c7d262fe445d6b235
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-02-12 12:07:16 -0300

    refactor: courseDataDto

--- src/dto/course.dto.ts
+++ src/dto/course.dto.ts
@@ -1,30 +1,30 @@
 import { IsString, IsNumber, IsUrl, MinLength, MaxLength, IsNotEmpty } from 'class-validator';
 
-export class CursoDto {
+export class CourseDataDto {
     @IsString({ message: 'O nome deve ser uma string.' })
     @IsNotEmpty({ message: 'O nome não pode estar vazio.' })
     @MinLength(2, { message: 'O nome deve ter pelo menos 2 caracteres.' })
     @MaxLength(200, { message: 'O nome não pode ter mais de 200 caracteres.' })
-    nome: string;
+    name: string;
 
     @IsNotEmpty({ message: 'A URL da foto não pode estar vazia.' })
     @IsString({ message: 'A URL da foto deve ser uma string.' })
     @IsUrl({}, { message: 'A URL da foto deve ser válida.' })
-    url_foto: string;
+    url: string;
 
     @IsNumber({}, { message: 'O preço deve ser um número.' })
     @IsNotEmpty({ message: 'O preço não pode estar vazio.' })
-    preco: number;
+    price: number;
 
     @IsNumber({}, { message: 'O preço com desconto deve ser um número.' })
     @IsNotEmpty({ message: 'O preço com desconto não pode estar vazio.' })
-    preco_com_desconto: number;
+    descontedPrice: number;
 
-    @IsString({ message: 'A carga horária deve ser uma string.' })
+    @IsString({ message: 'A carga horária deve ser em horas. Ex: 1.5' })
     @IsNotEmpty({ message: 'A carga horária não pode estar vazia.' })
-    carga_horaria: string;
+    workload: number;
 
     @IsString({ message: 'O conteúdo deve ser uma string.' })
     @IsNotEmpty({ message: 'O conteúdo não pode estar vazio.' })
-    conteudo: string;
+    content: string;
 }
