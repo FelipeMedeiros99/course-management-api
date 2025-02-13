278eb01d801ab14bc84067ce37cda2f0c4cbc9e7
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-02-13 00:02:13 -0300

    cleanup: comments removed

--- src/shoppingCart/shoppingCart.controller.ts
+++ src/shoppingCart/shoppingCart.controller.ts
@@ -39,48 +39,4 @@ export class shoppingCartController {
     await this.shoppingCartService.finalizePurchase(userCart);
     return "Purchase completed"
   }
-
-  // @Post("carrinho")
-  // @HttpCode(202)
-  // async adicionarAoCarrinho(@Body() dadosCarrinho: CarrinhoDto){
-  //     try{
-  //         return this.carrinhoService.adicionarAoCarrinho(dadosCarrinho)
-  //     }catch(error){
-  //         throw new Error("Erro ao adicionar ao carrinho: " + error.message);
-  //     }
-  // }
-
-
-  // @Get("carrinho/:id")
-  // @HttpCode(202)
-  // async buscarCarrinhos(@Param('id') id: number){
-  //     console.log(id)
-  //     try{
-  //         return this.carrinhoService.verCarrinho(id)
-  //     }catch(error){
-  //         throw new Error("Erro ao adicionar ao carrinho: " + error.message);
-  //     }
-  // }
-
-
-  // @Delete("carrinho")
-  // @HttpCode(202)
-  // async removerDoCarrinho(@Query() dados: IdCarrinhoDto){
-  //     try{
-  //         return this.carrinhoService.removerDoCarrinho(dados)
-  //     }catch(error){
-  //         throw new Error("Erro ao adicionar ao carrinho: " + error.message);
-  //     }
-  // }
-
-  // @Put("carrinho")
-  // @HttpCode(214)
-  // async alterarComoPago(@Query() dados: IdCarrinhoDto){
-  //     try{
-  //         return this.carrinhoService.alterarStatusDeCompra(dados)
-  //     }catch(e){
-  //         throw new Error("Erro ao tentar retornar status de compra: "+ e)
-  //     }
-  // }
-
 }
